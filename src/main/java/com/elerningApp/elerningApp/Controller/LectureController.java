b55b8ededc5d9ff604a59347a45abfc3e0e721f2
Omar <nyirurugoomar@gmail.com>
2025-02-22 17:55:46 +0200

    Done with lecture api

--- src/main/java/com/elerningApp/elerningApp/Controller/LectureController.java
+++ src/main/java/com/elerningApp/elerningApp/Controller/LectureController.java
@@ -1,11 +1,16 @@
 package com.elerningApp.elerningApp.Controller;
 
 import java.util.List;
+import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +35,23 @@ public class LectureController {
     @GetMapping
     public List<Lecture> getLectures(){
         return lectureService.getLectures();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Lecture> getLectureById(@PathVariable String id){
+        Optional<Lecture> lecture = lectureService.getLectureById(id);
+        return lecture.map(ResponseEntity::ok)
+               .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Lecture> updateLecture(@PathVariable String id, @RequestBody Lecture lecture){
+        return lectureService.updateLecture(id , lecture);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteLecture(@PathVariable String id){
+        lectureService.deleteLecture(id);
     }
     
 }
--- src/main/java/com/elerningApp/elerningApp/Service/LectureService.java
+++ src/main/java/com/elerningApp/elerningApp/Service/LectureService.java
@@ -1,8 +1,10 @@
 package com.elerningApp.elerningApp.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.elerningApp.elerningApp.Model.Lecture;
@@ -21,5 +23,22 @@ public class LectureService {
     public List<Lecture> getLectures(){
         return lectureRepo.findAll();
     }
+
+    public Optional<Lecture> getLectureById(String id){
+        return lectureRepo.findById(id);
+    }
+
+    public ResponseEntity<Lecture> updateLecture(String id, Lecture updatedLecture){
+        return lectureRepo.findById(id).map(existingLecture -> {
+            existingLecture.setLectureName(updatedLecture.getLectureName());
+            existingLecture.setLectureDescription(updatedLecture.getLectureDescription());
+            existingLecture.setMaterialUrl(updatedLecture.getMaterialUrl());
+            lectureRepo.save(existingLecture);
+            return ResponseEntity.ok(existingLecture);
+        }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
     
+    public void deleteLecture(String id){
+        lectureRepo.deleteById(id);
+    }
 }
